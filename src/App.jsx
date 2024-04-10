2ce903ddae9ec5f41625e53c563f2307fb6beb7f
moolraaj <raaj73906@gmail.com>
2024-04-10 18:29:28 +0530

    modify api.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
  
 import './App.css'
+import Api from './components/Api'
 
 function App() {
  
@@ -7,6 +8,8 @@ function App() {
   return (
     <>
        <h1>this is basic react app</h1>
+       <Api/>
+       
     </>
   )
 }
---
+++ src/components/Api.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+function Api() {
+    const [result, setResult] = useState([])
+    useEffect(()=>{
+        
+        let loadData=async()=>{
+            let data=await fetch('https://www.eligocs.com/wp-json/wp/v2/posts')
+            let resp=await data.json()
+            setResult(resp)
+            console.log(resp)
+        }
+        loadData()
+    },[])
+
+
+  return (
+     <>
+     <h1>this is a heading</h1>
+     {
+       result.map((ele)=>{
+        return <ul>
+            <li>{ele.comment_status}</li>
+        </ul>
+       }) 
+     }
+     </>
+  )
+}
+
+export default Api
