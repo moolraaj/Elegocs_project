9abee0d5a14339db13dfbeb1c9b690c953218780
Mr-rakesh-rk975 <rkraeksh24726@example.com>
2024-04-11 18:15:55 +0530

    new update

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-import AboutUs from "../../components/About/AboutUs"
-=======
-import AboutUs from "../../components/AboutUs"
+import AboutUs from '../../components/About/AboutUs'
 import './AboutPage.scss'
->>>>>>> 0ba9b5bea47424a93008e16e852ece3053053764
 
 function AboutPage() {
   return (
     <>
-<<<<<<< HEAD
     <AboutUs/>
     </>
-=======
-    <div>AboutPage</div>
-    <AboutUs/>
-        </>
->>>>>>> 0ba9b5bea47424a93008e16e852ece3053053764
+  
   )
 }
 
